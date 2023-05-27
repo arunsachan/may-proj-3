c1ef140f43aadb5f2cad88694cea449f28f18aff
arunsachan <66840949+arunsachan@users.noreply.github.com>
2023-05-27 23:11:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let data = [];
 
-
 fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
 .then(response => response.json())
 .then(dataResponse => {
